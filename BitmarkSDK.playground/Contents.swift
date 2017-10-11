82125ddc40d0e440e530bdb5c46058b87236ee15
Anh Nguyen <anhnguyen@bitmark.com>
2017-10-11 17:18:35 +0700

    Add file util: compute fingerprint, encrypt assets using chacha20

--- BitmarkSDK.playground/Contents.swift
+++ BitmarkSDK.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import BitmarkSDK
+import TweetNaclSwift
 
 let seed = try Seed(fromBase58: "5XEECsKPsXJEZRQJfeRU75tEk72WMs87jW1x9MhT6jF3UxMVaAZ7TSi")
 let seedCombine = seed.base58String
@@ -8,6 +9,20 @@ let network = seed.network.name
 let core = seed.core.hexEncodedString
 
 let seed1 = try Seed(version: 1, network: Config.liveNet)
-seed1.base58String
 let seed2 = try Seed(fromBase58: seed1.base58String)
-seed2.version
+
+let randomData = Common.randomBytes(length: 32)!
+let fingerPrint1 = FileUtil.Fingerprint.computeFingerprint(data: randomData)
+
+let fileURL = Bundle.main.url(forResource: "test", withExtension: ".txt")!
+let sessionKey = Common.randomBytes(length: 32)!
+
+do {
+    let secretKey = try TweetNaclSwift.NaclSign.KeyPair.keyPair()
+    let fingerPrint2 = try FileUtil.Fingerprint.computeFingerprint(fromFile: fileURL)
+    let encryption = try FileUtil.Encryption.encryptFile(fromFile: fileURL, sessionKey: sessionKey, secretKey: secretKey.secretKey)
+    print(encryption.hexEncodedString)
+}
+catch let e {
+    print(e)
+}
---
+++ BitmarkSDK/Utils/FileUtil.swift
@@ -0,0 +1,73 @@
+//
+//  FileUtil.swift
+//  BitmarkSDK
+//
+//  Created by Anh Nguyen on 10/11/17.
+//  Copyright © 2017 Bitmark. All rights reserved.
+//
+
+import Foundation
+import CryptoSwift
+import TweetNaclSwift
+
+public struct FileUtil {
+    public enum FileUtilError: Error {
+        case randomFailed
+        case openFileFailed
+        case sha3ChunkFailed
+    }
+    
+    public struct Fingerprint {
+        public static func computeFingerprint(data: Data) -> String {
+            let sha3 = SHA3(variant: .sha512)
+            let sha3Data = sha3.calculate(for: [UInt8](data))
+            return "01" + Data(bytes: sha3Data).hexEncodedString
+        }
+        
+        public static func computeFingerprint(fromFile url: URL) throws -> String {
+            let data = try Data(contentsOf: url)
+            return computeFingerprint(data: data)
+        }
+        
+//        public static func computeFingerprint(withChunkFromFilePath url: URL, chunkSize: Int = 1024) throws -> String {
+//            var sha3 = SHA3(variant: .sha512)
+//            let fileHandler = try FileHandle(forReadingFrom: url)
+//
+//            var sha3Bytes = [UInt8]()
+//
+//            var shouldContinue = true
+//
+//            while shouldContinue {
+//                let chunkData = fileHandler.readData(ofLength: chunkSize)
+//                if chunkData.count < chunkSize {
+//                    shouldContinue = false
+//                }
+//
+//                sha3Bytes += try sha3.update(withBytes: [UInt8](chunkData))
+//            }
+//
+//            sha3Bytes += try sha3.finish()
+//            return "01" + Data(bytes: sha3Bytes).hexEncodedString
+//        }
+    }
+    
+    public struct Encryption {
+        public static func encryptFile(fromFile url: URL, sessionKey: Data, secretKey: Data) throws -> Data {
+            let fileContent = try Data(contentsOf: url)
+            
+            guard let nonce = Common.randomBytes(length: 12) else {
+                throw(FileUtilError.randomFailed)
+            }
+            
+            let cipher = try ChaCha20(key: [UInt8](sessionKey), iv: [UInt8](nonce))
+            
+            let encryptedBytes = try cipher.encrypt([UInt8](fileContent))
+            var encryptedData = Data(bytes: encryptedBytes)
+            
+            let signature = try TweetNaclSwift.NaclSign.signDetached(message: fileContent, secretKey: secretKey)
+            encryptedData += signature
+            
+            return encryptedData
+        }
+    }
+}
