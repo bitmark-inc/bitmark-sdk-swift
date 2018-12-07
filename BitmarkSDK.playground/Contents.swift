e47f1958a60ab1598ad35b3944cb747e507ffbc1
Anh Nguyen <anhnguyen@bitmark.com>
2018-12-07 16:50:45 +0700

    Enable public encryption function for Account

--- BitmarkSDK.playground/Contents.swift
+++ BitmarkSDK.playground/Contents.swift
@@ -10,14 +10,14 @@ let fileURL = Bundle.main.url(forResource: "test", withExtension: ".txt")!
 
 do {
     BitmarkSDK.initialize(config: SDKConfig(apiToken: "bmk-lljpzkhqdkzmblhg", network: .testnet, urlSession: URLSession.shared))
-    let account = try Account(version: .v2, network: .livenet)
-    let seed1 = try account.toSeed()
-    let recovery = try account.getRecoverPhrase(language: .chineseTraditional)
-    print(seed1)
-    print(recovery)
     
-    let account2 = try Account(recoverPhrase: recovery, language: .chineseTraditional)
-    print(try account2.toSeed())
+    let phrase = ["abuse", "tooth", "riot", "whale", "dance", "dawn", "armor", "patch", "tube", "sugar", "edit", "clean",
+                  "guilt", "person", "lake", "height", "tilt", "wall", "prosper", "episode", "produce", "spy", "artist", "account"]
+    
+    let (account, bitmarkids) = try Migration.migrate(recoverPhrase: phrase, language: .english)
+    print(account.accountNumber)
+    print(try account.getRecoverPhrase(language: .english))
+    print(bitmarkids)
 }
 catch let e {
     print(e)
