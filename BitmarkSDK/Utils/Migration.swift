075d987eb8339bbce3a6c49c2fde7a1cf696526b
Anh Nguyen <anhnguyen@bitmark.com>
2019-02-14 11:51:03 +0700

    Disable migrate function

--- BitmarkSDK/Utils/Migration.swift
+++ BitmarkSDK/Utils/Migration.swift
@@ -9,62 +9,62 @@
 import Foundation
 
 public struct Migration {
-    public static func migrate(recoverPhrase: [String], language: RecoveryLanguage) throws -> (Account, [String]) {
-        let accountToMigrate = try Account(recoverPhrase: recoverPhrase, language: language)
-        if accountToMigrate.seed.version != .v1 {
-            throw("Only support migration from bitmark account version 1")
-        }
-        
-        // Query for current owning bitmarks
-        var lastOffset: Int64? = nil
-        var owningBitmarks = [Bitmark]()
-        var shouldContinue = true
-        while shouldContinue {
-            var bitmarksQuery = Bitmark.newBitmarkQueryParams()
-                .owned(by: accountToMigrate.address)
-                .loadAsset(true)
-                .to(direction: .earlier)
-                .includePending(true)
-            
-            if let lastOffset = lastOffset {
-                bitmarksQuery = bitmarksQuery.at(lastOffset)
-            }
-            
-            let (bitmarks, _) = try Bitmark.list(params: bitmarksQuery)
-            
-            if bitmarks == nil || bitmarks?.count == 0 {
-                break
-            }
-            
-            if bitmarks!.count < 100 {
-                shouldContinue = false
-            }
-            
-            lastOffset = bitmarks!.last!.offset
-            
-            owningBitmarks += bitmarks!
-        }
-        
-        // Group bitmarks with their asset_id
-        let assetBitmarkCountMap = Dictionary(grouping: owningBitmarks,
-                                         by: { $0.asset_id })
-            .mapValues( { $0.count } )
-        
-        // Create a new bitmark account v2
-        let newAccount = try Account(version: .v2)
-        
-        // Reissue new bitmarks with new account
-        var bitmarkIds = [String]()
-        
-        for (assetId, count) in assetBitmarkCountMap {
-            var issuanceParam = try Bitmark.newIssuanceParams(assetID: assetId, owner: newAccount.accountNumber, quantity: count)
-            try issuanceParam.sign(accountToMigrate)
-            let result = try Bitmark.issue(issuanceParam)
-            bitmarkIds += result
-        }
-        
-        return (newAccount, bitmarkIds)
-    }
+//    public static func migrate(recoverPhrase: [String], language: RecoveryLanguage) throws -> (Account, [String]) {
+//        let accountToMigrate = try Account(recoverPhrase: recoverPhrase, language: language)
+//        if accountToMigrate.seed.version != .v1 {
+//            throw("Only support migration from bitmark account version 1")
+//        }
+//
+//        // Query for current owning bitmarks
+//        var lastOffset: Int64? = nil
+//        var owningBitmarks = [Bitmark]()
+//        var shouldContinue = true
+//        while shouldContinue {
+//            var bitmarksQuery = Bitmark.newBitmarkQueryParams()
+//                .owned(by: accountToMigrate.address)
+//                .loadAsset(true)
+//                .to(direction: .earlier)
+//                .includePending(true)
+//
+//            if let lastOffset = lastOffset {
+//                bitmarksQuery = bitmarksQuery.at(lastOffset)
+//            }
+//
+//            let (bitmarks, _) = try Bitmark.list(params: bitmarksQuery)
+//
+//            if bitmarks == nil || bitmarks?.count == 0 {
+//                break
+//            }
+//
+//            if bitmarks!.count < 100 {
+//                shouldContinue = false
+//            }
+//
+//            lastOffset = bitmarks!.last!.offset
+//
+//            owningBitmarks += bitmarks!
+//        }
+//
+//        // Group bitmarks with their asset_id
+//        let assetBitmarkCountMap = Dictionary(grouping: owningBitmarks,
+//                                         by: { $0.asset_id })
+//            .mapValues( { $0.count } )
+//
+//        // Create a new bitmark account v2
+//        let newAccount = try Account(version: .v2)
+//
+//        // Reissue new bitmarks with new account
+//        var bitmarkIds = [String]()
+//
+//        for (assetId, count) in assetBitmarkCountMap {
+//            var issuanceParam = try Bitmark.newIssuanceParams(assetID: assetId, owner: newAccount.accountNumber, quantity: count)
+//            try issuanceParam.sign(accountToMigrate)
+//            let result = try Bitmark.issue(issuanceParam)
+//            bitmarkIds += result
+//        }
+//
+//        return (newAccount, bitmarkIds)
+//    }
     
     // Query and transfer all of current confirmed properties
     public static func rekey(from accountFrom: Account, to accountTo: Account) throws {
