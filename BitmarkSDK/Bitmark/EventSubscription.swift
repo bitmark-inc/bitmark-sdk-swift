481c55913d533eed34e863b02d75093331e83d56
Anh Nguyen <anhnguyen@bitmark.com>
2019-06-25 16:47:44 +0700

    Add a dummy project for CentrifugeSwift since it doesn't have a .xcproject

--- BitmarkSDK/Bitmark/EventSubscription.swift
+++ BitmarkSDK/Bitmark/EventSubscription.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftCentrifuge
 
 public struct BitmarkChangedInfo {
     public let bitmarkId: String
---
+++ SwiftCentrifuge/SwiftCentrifuge/SwiftCentrifuge.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCentrifuge.h
+//  SwiftCentrifuge
+//
+//  Created by Anh Nguyen on 6/25/19.
+//  Copyright © 2019 Centrifuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCentrifuge.
+FOUNDATION_EXPORT double SwiftCentrifugeVersionNumber;
+
+//! Project version string for SwiftCentrifuge.
+FOUNDATION_EXPORT const unsigned char SwiftCentrifugeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCentrifuge/PublicHeader.h>
+
+
