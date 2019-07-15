5a672da1a75e81b6bd7917299ecf678ff41afbc7
Anh Nguyen <anhnguyen@bitmark.com>
2019-07-15 16:39:12 +0700

    Update rekey function reflects changes from core-api

--- BitmarkSDK/Utils/Migration.swift
+++ BitmarkSDK/Utils/Migration.swift
@@ -105,22 +105,28 @@ public struct Migration {
             } else {
                 for i in 0..<owningBitmarks.count {
                     let bitmark = owningBitmarks[i]
-
-                    var transferRequest = TransferRequest()
-                    transferRequest.set(fromOwner: accountFrom.getAccountNumber())
-                    try transferRequest.set(to: accountTo.getAccountNumber())
-                    transferRequest.set(fromTx: bitmark.id)
-                    transferRequest.set(requireCountersignature: true)
-                    try transferRequest.sign(accountFrom)
-
-                    var counterTransfer = CountersignedTransferRequest(link: bitmark.id,
-                                                                       owner: transferRequest.owner!,
-                                                                       signature: transferRequest.signature!.hexEncodedString)
-                    try counterTransfer.sign(accountTo)
-
-                    let api = API()
-                    _ = try api.transfer(withCounterTransfer: counterTransfer)
-
+                    
+                    var params = try Bitmark.newOfferParams(to: accountTo.getAccountNumber(), info: nil)
+                    try params.from(bitmarkID: bitmark.id)
+                    try params.sign(accountFrom)
+                    try Bitmark.offer(withOfferParams: params)
+                }
+                
+                let query = Bitmark.newBitmarkQueryParams()
+                    .offerTo(accountTo.getAccountNumber())
+                let (bs, _) = try Bitmark.list(params: query)
+                
+                guard let offeringBitmarks = bs else {
+                    handler?(0, "No bitmark found")
+                    return
+                }
+                
+                for i in 0..<offeringBitmarks.count {
+                    let bitmark = offeringBitmarks[i]
+                    var responseParams = try Bitmark.newTransferResponseParams(withBitmark: bitmark, action: .accept)
+                    try responseParams.sign(accountTo)
+                    _ = try Bitmark.respond(withResponseParams: responseParams)
+                    
                     // Update progress
                     handler?(Float((i + 1)) / Float(owningBitmarks.count), nil)
                 }
