4e717c7d33f6604690ec5d7c4c5969e3040329d3
Anh Nguyen <anhnguyen@bitmark.com>
2019-08-09 11:32:55 +0700

    Replace new API v5's test token

--- BitmarkSDKTests/Asset_Tests.swift
+++ BitmarkSDKTests/Asset_Tests.swift
@@ -51,7 +51,7 @@ class Asset_Tests: XCTestCase {
     }
     
     override func setUp() {
-        BitmarkSDK.initialize(config: SDKConfig(apiToken: "bmk-lljpzkhqdkzmblhg", network: .testnet, urlSession: URLSession.shared))
+        BitmarkSDK.initialize(config: SDKConfig(apiToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk0NTQ1MDg5NzcsImlhdCI6MTU2NTMyNDk3NywiaXNzIjoiOVEvSllhVkJJOXZmRlp6N0J2VjRNdz09Iiwic3ViIjoiZVhjcHlKcXpxZGdWVk1YRVJMUmh2dEpxWGFWWnZoS0hkcEZLSzdlUnJ4UDlSTXM4cFYifQ.EiIXik49k-DTuRIRYK5nPm6qV9viZupYqlfLxr5n-3wkkABW55InRXnPWHlyDE0Fx0b1gcJg67eifVSNgnKrD6l2JdKEzDBvUfmj41OQxFTJ_hwA25wfYHudnwD_-axtv3LqREYu_jkxNKe6riw3IcX-ve3zvpDnFqA_Ntu75Z10-Hapkz9yhAVI_pp0CKo2qjKNjYB2s21PvQ2r2yifvt_xLTXth-aw61VzRKJfr1mmi97yHoay6Fo89sDmQpxaSxHRTKq2thyGWghXVz49iQozkthMwpMZZ5i9cSyAvr5a-wOcmI8-NZbKNv9RfBwd4bNm_hZFcXSfw_ZaLwmNNw", network: .testnet, urlSession: URLSession.shared))
     }
     
     // MARK:- Asset
